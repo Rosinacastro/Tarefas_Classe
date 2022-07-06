915b15a9821750e6deab4d74f9fdb5cde79282da
Rosinacastro <rosicastro1@live.com>
2022-07-06 10:52:29 -0300

    Enviando tarefa

---
+++ src/Aluno.java
@@ -0,0 +1,52 @@
+/**
+ * @author rosina.castro
+ */
+public class Aluno {
+
+    private int codigo;
+    private String nome;
+    private int cpf;
+    private String endereço;
+    private int telefone;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author rosina.castro
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+        System.out.println(aluno.getCodigo());
+        System.out.println(aluno.getCpf());
+        System.out.println(aluno.getEndereço());
+        System.out.println(aluno.getNome());
+        System.out.println(aluno.getTelefone());
+    }
+}
